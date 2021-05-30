b75ab23f8968ec9792378988057fcf1229d778d4
fangmin26 <fangmin_@naver.com>
2021-05-30 10:09:49 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://fangmin:alswl@cluster0.onvdd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected.."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
